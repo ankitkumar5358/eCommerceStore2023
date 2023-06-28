0ebed149cc3f5350ed36c4eac9aeb121c77469f7
Ankit Kumar <57992836+ankitkumar5358@users.noreply.github.com>
2023-06-28 09:42:33 +0530

    auth & context api, localstorage feature added for user & token

--- client/src/pages/Auth/Login.js
+++ client/src/pages/Auth/Login.js
@@ -4,10 +4,12 @@ import toast from 'react-hot-toast';
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import "../../styles/AuthStyles.css"
+import { useAuth } from "../../context/auth";
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [auth, setAuth] = useAuth();
 
   const navigate = useNavigate();
   const handleSubmit = async (e) => {
@@ -16,6 +18,12 @@ const Login = () => {
       const res = await axios.post(`${process.env.REACT_APP_API}/api/v1/auth/login`, { email, password });
       if (res.data.success) {
         toast.success(res.data.message);
+        setAuth({
+          ...auth,
+          user: res.data.user,
+          token: res.data.token
+        })
+        localStorage.setItem('auth', JSON.stringify(res.data))
         navigate('/');
       } else {
         toast.error(res.data.message);
@@ -28,38 +36,38 @@ const Login = () => {
 
   return (
     <Layout title={"Login - Ecommerce App"}>
-    <div className="form-container">
-    <h1 className="title">Login Form</h1>
-      <form onSubmit={handleSubmit}>
-        <div className="mb-3">
-          <input
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            className="form-control"
-            id="exampleInputEmail"
-            placeholder="Enter Your Email"
-            required
-          />
-        </div>
+      <div className="form-container">
+        <h1 className="title">Login Form</h1>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-3">
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="form-control"
+              id="exampleInputEmail"
+              placeholder="Enter Your Email"
+              required
+            />
+          </div>
 
-        <div className="mb-3">
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            className="form-control"
-            id="exampleInputPassword"
-            placeholder="Enter Your Password"
-            required
-          />
-        </div>
+          <div className="mb-3">
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="form-control"
+              id="exampleInputPassword"
+              placeholder="Enter Your Password"
+              required
+            />
+          </div>
 
-        <button type="submit" className="btn btn-primary">
-          Login
-        </button>
-      </form>
-    </div>
+          <button type="submit" className="btn btn-primary">
+            Login
+          </button>
+        </form>
+      </div>
     </Layout>
   );
 }
--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import Layout from '../components/Layout/Layout'
+import { useAuth } from '../context/auth'
 
 const HomePage = () => {
+  const [auth] = useAuth()
   return (
     <Layout title={"Home - Shop Now"}>
-        <h1>HomePage</h1>
+      <h1>HomePage</h1>
+      <pre> {JSON.stringify(auth, null, 4)} </pre>
     </Layout>
   )
 }
